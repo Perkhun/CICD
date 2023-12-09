5764f13da498ccbc40b15678b43f398d6aa8d1df
Perkhun <90705936+Perkhun@users.noreply.github.com>
2023-12-09 18:58:05 +0200

    Update product.test.js

---
+++ _ _ tests _ _/product.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const express = require("express");
+const app = require("./index"); // Імпортуємо налаштований сервер
+
+test("POST /products додає новий продукт", async () => {
+  const response = await request(app)
+    .post("/products")
+    .send({ name: "Тестовий продукт", price: 10 });
+
+  expect(response.status).toBe(201);
+  expect(response.body).toHaveProperty("message", "Product added successfully.");
+  expect(response.body).toHaveProperty("productId");
+});
