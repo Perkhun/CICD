b236717f8cb1c182315ea91419ae9d5c69c62f09
Perkhun <90705936+Perkhun@users.noreply.github.com>
2023-12-09 21:39:26 +0200

    Update product.test.js

--- _ _ tests _ _/product.test.js
+++ _ _ tests _ _/product.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const express = require("express");
-const app = require("../index"); // Імпортуємо налаштований сервер
+const app = require("../index");
 
 test("POST /products додає новий продукт", async () => {
   const response = await request(app)
@@ -10,4 +10,9 @@ test("POST /products додає новий продукт", async () => {
   expect(response.status).toBe(201);
   expect(response.body).toHaveProperty("message", "Product added successfully.");
   expect(response.body).toHaveProperty("productId");
+});
+
+// Закриваємо сервер, коли тести виконані
+afterAll(() => {
+  app.close();  // Можливо, вам потрібно використовувати іменно app.close, в залежності від вашої конфігурації
 });
